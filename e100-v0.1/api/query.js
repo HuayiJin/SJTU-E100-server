aeb8198df67b58670623356e181c64e77d07ee23
HY J <joseph.j@HYdeMacBook-Pro-2.local>
2019-11-12 14:15:06 +0800

    update some api and test them

--- e100-v0.1/api/query.js
+++ e100-v0.1/api/query.js
@@ -1,34 +1,44 @@
 var sqlquery = require('./sqlquery');
 var safetycheck = require('./safetycheck');
+const db_NAME = "e100";
+const tb_NAME = "E100_TABLE_TEST";
+const GLOBAL_limit = 1000
 
 // MySQL数据库联接配置
 var mysql = require('mysql');
 var pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: '123456',
-    database: 'localtest'
+    password: '',
+    database: 'e100'
 });
 
 function db(sql, callback) {
-    pool.getConnection(function (err, connection) {
-        // Use the connection
-        connection.query(sql, function (err, rows) {
-            if(err){
-                console.log(err);
-            }
-            connection.release();//释放链接
-            callback(err, rows);
+    try {
+        pool.getConnection(function (err, connection) {
+            // Use the connection
+            connection.query(sql, function (err, rows) {
+                connection.release();//释放链接
+                callback(err, rows);
+            });
         });
-    });
+    } catch (error) {
+        console.log(error)
+        callback(error, null)
+    }
 }
 
-exports.get_all_status = function (req, res) {
-    console.log(req.query);
-    var localsql = 
-        "select *,max(timestamp) from CAR_REALTIME_DATA_TEST group by car_VIN";
-    console.log('localsql is ' + localsql);
+/*============================全局============================*/
 
+//请求所有车辆的当前所有数据(测试用)
+exports.get_all_all = function (req, res) {
+    console.log(req.query);
+    var localsql =
+        "SELECT a.* FROM " + tb_NAME + 
+        " a INNER JOIN (select car_VIN, max(create_time) from " + tb_NAME +
+        " GROUP BY car_VIN) b ON a.car_VIN = b.car_VIN and a.create_time = b.`max(create_time)`;"
+
+    console.log('localsql is ' + localsql);
     db(localsql, function (err, resdata) {
         if (err) {
             res.end("查询失败：", err)
@@ -39,119 +49,166 @@ exports.get_all_status = function (req, res) {
     });
 }
 
-
-/**
- * 查询列表页
- */
-exports.getVehicles = function (req, res, next) {
-    db(sqlquery.getVehicles, function (err, resdata) {
-        console.log('==========');
-        if (err) {
-            res.render('users.html', { title: 'Express', datas: [] });  // this renders "views/users.html"
-        } else {
-            res.render('users.html', { title: 'Express', datas: resdata });
-        }
-    })
-};
-
-
-/**
- * 查询
- */
-exports.searchVehicleRegister = function (req, res) {
-    console.log(req.query);
-    var localsql = safetycheck.searchRegister(req, sqlquery.searchVehiclebyVIN_Register, sqlquery.searchVehiclebyNUM_Register);
-    console.log('localsql is ' + localsql);
+//请求所有车辆的当前状态（用于计算饼图）
+exports.get_all_status = function (req, res) {
+    var localsql = 
+        "SELECT a.car_VIN, a.create_time, a.vehicle_operating_mode FROM " + tb_NAME +
+        " a INNER JOIN (select car_VIN, max(create_time) from " + tb_NAME +
+        " GROUP BY car_VIN) b ON a.car_VIN = b.car_VIN and a.create_time = b.`max(create_time)`;"
 
     db(localsql, function (err, resdata) {
         if (err) {
             res.end("查询失败：", err)
         } else {
-            //console.log(resdata);
             res.status(200).send(resdata);
         }
     });
 }
 
-exports.searchVehicleRuntime = function (req, res) {
+//请求所有车辆的历史状态（计算多级折线图）
+exports.get_history_status = function (req, res) {
     console.log(req.query);
-    var localsql = safetycheck.searchHistory(req, sqlquery.searchVehiclebyVIN_Runtime, sqlquery.searchVehiclebyNUM_Runtime);
-    console.log('localsql is ' + localsql);
+    var limit = 100
+    if (req.query.limit) limit = req.query.limit;
+
+    var localsql = 
+        "SELECT a.car_VIN, a.create_time, a.vehicle_operating_mode FROM " + tb_NAME +
+        " a INNER JOIN (select car_VIN, max(create_time) from " + tb_NAME +
+        " GROUP BY car_VIN) b ON a.car_VIN = b.car_VIN and a.create_time = b.`max(create_time)`;"
+
     db(localsql, function (err, resdata) {
         if (err) {
-            console.log(err)
             res.end("查询失败：", err)
         } else {
-            subresdata = []
-            gap = 1
-            if(resdata.length > 220){gap = parseInt(resdata.length / 200)}
-            for (i=0; i<resdata.length; i+= gap){
-                //console.log(i)
-                subresdata.push(resdata[i])
+            var res_group = []
+            var counter = 0;
+            for (let i = 0; i < resdata.length; i++) {
+                res_group.push({car_VIN:resdata[i].car_VIN})
+                res_group[i].latest_info = {
+                    create_time: resdata[i].create_time,
+                    vehicle_operating_mode: resdata[i].vehicle_operating_mode
+                }
             }
-            res.status(200).send(subresdata);
+            
+            for (let i = 0; i < res_group.length; i++) {
+                const vin = res_group[i].car_VIN;
+                var lsql = "select create_time, vehicle_operating_mode from " + tb_NAME +
+                " where car_VIN = \"" + vin +
+                "\" and create_time > date_sub(current_timestamp(), interval " + limit + 
+                " minute) limit " + GLOBAL_limit;                  
+                db(lsql, function (err, sql_result) {
+                    if (err) {
+                        console.log(err)
+                    } else {
+                        res_group[i].info = sql_result
+                    }
+                    counter ++
+                    if(counter == res_group.length){
+                        res.status(200).send(res_group);
+                    }
+                })
+            }
         }
     });
 }
 
-exports.searchVehicleBattery = function (req, res) {
+//请求所有(在线、离线、充电)车辆的当前位置
+//请求所有车辆的历史状态（计算多级折线图）
+exports.get_all_location = function (req, res) {
     console.log(req.query);
-    var localsql = safetycheck.searchHistory(req, sqlquery.searchVehiclebyVIN_Battery, sqlquery.searchVehiclebyNUM_Battery);
-    console.log('localsql is ' + localsql);
+    var limit = 100
+    if (req.query.limit) limit = req.query.limit;
+
+    var localsql = 
+        "SELECT a.car_VIN, a.create_time, a.vehicle_operating_mode FROM " + tb_NAME +
+        " a INNER JOIN (select car_VIN, max(create_time) from " + tb_NAME +
+        " GROUP BY car_VIN) b ON a.car_VIN = b.car_VIN and a.create_time = b.`max(create_time)`;"
 
     db(localsql, function (err, resdata) {
         if (err) {
             res.end("查询失败：", err)
         } else {
-            subresdata = []
-            gap = 1
-            if(resdata.length > 220){gap = parseInt(resdata.length / 200)}
-            for (i=0; i<resdata.length; i+= gap){
-                //console.log(i)
-                subresdata.push(resdata[i])
+            var res_group = []
+            var counter = 0;
+            for (let i = 0; i < resdata.length; i++) {
+                res_group.push({car_VIN:resdata[i].car_VIN})
+                res_group[i].latest_info = {
+                    create_time: resdata[i].create_time,
+                    vehicle_operating_mode: resdata[i].vehicle_operating_mode
+                }
             }
-            res.status(200).send(subresdata);
+            
+            for (let i = 0; i < res_group.length; i++) {
+                const vin = res_group[i].car_VIN;
+                var lsql = "select create_time, vehicle_operating_mode from " + tb_NAME +
+                " where car_VIN = \"" + vin +
+                "\" and create_time > date_sub(current_timestamp(), interval " + limit + 
+                " minute) limit " + GLOBAL_limit;                  
+                db(lsql, function (err, sql_result) {
+                    if (err) {
+                        console.log(err)
+                    } else {
+                        res_group[i].info = sql_result
+                    }
+                    counter ++
+                    if(counter == res_group.length){
+                        res.status(200).send(res_group);
+                    }
+                })
+            }
         }
     });
 }
 
-exports.searchVehicleAlert = function (req, res) {
-    console.log(req.query);
-    var localsql = safetycheck.searchHistory(req, sqlquery.searchVehiclebyVIN_Alert, sqlquery.searchVehiclebyNUM_Alert);
-    console.log('localsql is ' + localsql);
+/*============================单车============================*/
 
+//请求单车当前所有信息
+exports.get_single_info = function (req, res) {
+    console.log(req.query);
+    var localsql = 
+        ""
+
+    console.log('localsql is ' + localsql);
     db(localsql, function (err, resdata) {
         if (err) {
             res.end("查询失败：", err)
         } else {
-            //console.log(resdata);
+            console.log(resdata);
             res.status(200).send(resdata);
         }
     });
 }
 
-exports.searchVehicleOther = function (req, res) {
+//请求单车某项历史记录
+exports.get_single_history = function (req, res) {
     console.log(req.query);
-    var localsql = safetycheck.searchHistory(req, sqlquery.searchVehiclebyVIN_Other, sqlquery.searchVehiclebyNUM_Other);
-    console.log('localsql is ' + localsql);
+    var localsql = 
+        ""
 
+    console.log('localsql is ' + localsql);
     db(localsql, function (err, resdata) {
         if (err) {
             res.end("查询失败：", err)
         } else {
-            subresdata = []
-            gap = 1
-            if(resdata.length > 220){gap = parseInt(resdata.length / 200)}
-            for (i=0; i<resdata.length; i+= gap){
-                //console.log(i)
-                subresdata.push(resdata[i])
-            }
-            res.status(200).send(subresdata);
+            console.log(resdata);
+            res.status(200).send(resdata);
         }
     });
 }
 
+//请求单车历史轨迹
+exports.get_single_route = function (req, res) {
+    console.log(req.query);
+    var localsql = 
+        ""
 
-
-
-
+    console.log('localsql is ' + localsql);
+    db(localsql, function (err, resdata) {
+        if (err) {
+            res.end("查询失败：", err)
+        } else {
+            console.log(resdata);
+            res.status(200).send(resdata);
+        }
+    });
+}
--- e100-v0.1/routes/index.js
+++ e100-v0.1/routes/index.js
@@ -9,37 +9,87 @@ router.get('/', function(req, res, next) {
   res.render('index.html', { title: 'ExpressTitle' });
 });
 
-/*========================================================*/
 
-//请求所有车辆的当前状态（用于计算饼图）
+/*============================全局============================*/
+
+/**请求所有车辆的当前所有数据(测试用)
+ * res = [{
+ *      car_VIN = ...
+ *      create_time = ...
+ *      ...
+ * },{}...]
+ */
+router.get('/query/all/all', query.get_all_all);
+
+/**请求所有车辆的当前状态（用于计算饼图）
+ * res = [{
+ *      car_VIN = ...
+ *      create_time = ...
+ *      vehicle_operating_mode = ...
+ * },{}...]
+ */
 router.get('/query/all/status', query.get_all_status);
 
-//请求所有车辆的历史状态（计算多级折线图）
-//参数 length = [1,1000]
-//router.get('/query/history/status', query.get_history_status);
+/**请求所有车辆的历史状态（计算多级折线图）
+ * 参数 length = [1,1000] -> 按分钟为单位，距今的分钟数
+ * res = [{
+ *      car_VIN,
+ *      latest_info:{
+ *          create_time:
+ *          vehicle_operating_mode:
+ *          }
+ *      info:[{}{}{}...]
+ * },{}...]
+ */
+router.get('/query/history/status', query.get_history_status);
 
-//请求所有车辆的当前位置
-//参数 status = {online, offline, charging}
-//router.get('/query/all/location', query.get_all_location);
+/**请求所有(在线、离线、充电)车辆的当前位置
+ * 参数 status = {online, offline, charging}
+ * res = [{
+ *      car_VIN = ...
+ *      longitude = 121.425
+ *      latitude = 31.0198
+ * },{}...]
+ */
+router.get('/query/all/location', query.get_all_location);
 
-/*========================================================*/
 
-//请求单车当前所有信息
-//参数 vin = "LXXXXXXXXXXXXXXXXX"
-//router.get('/query/single/info', query.get_single_info);
+/*============================单车============================*/
 
-//请求单车某项历史记录
-//参数 vin = "LXXXXXXXXXXXXXXXXX"
-//参数 type = {speed, drive_motor_rpm, drive_motor_temperature ...}
-//参数 length = [1,1000]
-//router.get('/query/single/history', query.get_single_history);
+/**请求单车当前所有信息
+ * 参数 car_VIN = "LXXXXXXXXXXXXXXXXX"
+ * res = {
+ *      speed = 20
+ *      drive_motor_rpm = 30
+ *      ...
+ * }
+ */
+router.get('/query/single/info', query.get_single_info);
 
-//请求单车历史轨迹
-//参数 vin = "LXXXXXXXXXXXXXXXXX"
-//参数 length = [1,1000]
-//querrouter.get('/query/single/route', query.get_single_route);
+/**请求单车某项历史记录
+ * 参数 car_VIN = "LXXXXXXXXXXXXXXXXX"
+ * 参数 type = {speed, drive_motor_rpm, drive_motor_temperature ...}
+ * 参数 length = [1,1000]
+ * res = [{
+ *      create_time = ...
+ *      speed = 20
+ *      drive_motor_rpm = 30
+ *      ...
+ * },{}...]
+ */
+router.get('/query/single/history', query.get_single_history);
 
-/*========================================================*/
+/**请求单车历史轨迹
+ * 参数 car_VIN = "LXXXXXXXXXXXXXXXXX"
+ * 参数 length = [1,1000]
+ * res = [{
+ *      create_time = ...
+ *      longitude = 20
+ *      latitude = 30
+ *      ...
+ * },{}...]
+ */
+router.get('/query/single/route', query.get_single_route);
 
 
 module.exports = router;
